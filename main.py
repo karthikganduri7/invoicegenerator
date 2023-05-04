12722ca86d6934a250e5165ab9cd3fe590a86110
karthikganduri <karthikganduri@yahoo.in>
2023-05-04 19:05:40 +0530

    Added headed and total price, company and logo

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ for filepath in filepaths:
     pdf.cell(w=30, h=8, txt=str(columns[3]), border=1)
     pdf.cell(w=30, h=8, txt=str(columns[4]), border=1, ln=1)
 
+    price = 0
     # Add rows to table
     for index, row in df.iterrows():
-
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
@@ -41,5 +41,21 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=70, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=" ", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Total sum is {total_sum}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=25, h=8, txt=f"KarthikG")
+    pdf.image("photo.jpeg", w=10)
 
     pdf.output(f"PDFS/{filename}.pdf")
