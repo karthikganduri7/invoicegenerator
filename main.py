42a87618a31b64c635127e5d68c0028091c76c3d
karthikganduri <karthikganduri@yahoo.in>
2023-05-04 17:49:04 +0530

    added invoice date

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
-filepaths = glob.glob("invoices/*.xlsx")
 
+filepaths = glob.glob("invoices/*.xlsx")
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_no = filename.split("-")[0]
-    invoice_date = filename.split("-")[1]
+    invoice_no, invoice_date = filename.split("-")
+
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice no.{invoice_no}")
+    pdf.cell(w=50, h=8, txt=f"Invoice no.{invoice_no}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {invoice_date}")
     pdf.output(f"PDFS/{filename}.pdf")
